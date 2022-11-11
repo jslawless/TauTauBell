3c33fcd5ebf95bf4f0e12cd7d4cb2ac4336243bd
John Lawless <cms_utk@frizzle-07.nomad.utk.edu>
2022-11-11 10:17:24 -0500

    Fixed the converter to do vertexing correctly. At some point I will refactor this into a method, but this works for now.

--- converter.py
+++ converter.py
@@ -6,7 +6,6 @@ from ROOT import gROOT, gBenchmark
 import os
 
 
-#Using a list here instead of a numpy array as I'm pretty sure that lists are faster at appending which we're doing
 f = open("data/e2E2e3E3_H250ISR1BS1_LR_addH.lhe_tauDecaysigsig_mixingAngle0.hepevt")
 
 line = f.readline()
@@ -15,6 +14,7 @@ evt_num = 0
 writer = hep.WriterAscii("data/LR0.hepmc")
 
 while line:
+#while evt_num < 4: 
     #print(line)
     numP = int(line)
     px = []
@@ -25,7 +25,11 @@ while line:
     pid = []
     sta = []
     parents = []
-
+    children = []
+    vx = []
+    vy = []
+    vz = []
+    vt = []
 
     evt = hep.GenEvent()
      
@@ -33,18 +37,22 @@ while line:
         line = f.readline()
         properties = line.split(' ')
         #print(properties)
-        fv = hep.FourVector(float(properties[6]),float(properties[7]),float(properties[8]),float(properties[9]))
         px.append(float(properties[6]))
         py.append(float(properties[7]))
         pz.append(float(properties[8]))
         en.append(float(properties[9]))
-        m.append(fv.m())
+        m.append(float(properties[10]))
         pid.append(int(properties[1]))
         sta.append(int(properties[0]))
         parents.append([int(properties[2]),int(properties[3])])
-    
+        children.append([int(properties[4]),int(properties[5])])
+        vx.append(float(properties[11]))
+        vy.append(float(properties[12]))
+        vz.append(float(properties[13]))
+        vt.append(float(properties[14]))
+
     #IMPORTANT!! Must be on pyhepmc ~2.7.3 or something. This function is not present in 2.0.0
-    evt.from_hepevt(evt_num, px, py, pz, en, m, pid, sta, parents)
+    evt.from_hepevt(evt_num, px, py, pz, en, m, pid, sta, parents, children, vx, vy, vz, vt)
     print(evt)
     writer.write_event(evt)    
     line = f.readline()
