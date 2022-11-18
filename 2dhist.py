40a183ec9bec2857beb2855db6d9384685e04143
John Lawless <cms_utk@frizzle-07.nomad.utk.edu>
2022-11-18 13:51:08 -0500

    Boosted the taus into the reference frame of the Higgs. Added a class that pretty much does the same thing but has a 2D hist in it. Probably didnt need to do that in a second file now that I think about it but whatever.

--- 2dhist.py
+++ 2dhist.py
@@ -1,21 +1,24 @@
 import pyhepmc as hep
 import math
 import scipy.constants as sci
-from ROOT import TCanvas, TPad, TH1F, TFile
+from ROOT import TCanvas, TPad, TH1F, TH2F,TFile
 from ROOT import gROOT, gBenchmark, TLorentzVector
 import os
 
 
 #Set to UPDATE to leave previous files there
 #Open the .root file in RECREATE mode in order to wipe all histograms previously there
-myfile = TFile( 'RootFiles/startup_cutonZmass.root', 'RECREATE' )
+myfile = TFile( 'RootFiles/2Dhist.root', 'RECREATE' )
 
 #
 def histogram(directory, name, description):
     hist1 = TH1F( name + "_im", "Invariant Mass of Muon Pairs", 50, 0, 100 )
     hist2 = TH1F( name + "_tau_im", "Invariant Mass of tau Pairs", 100, 0, 200 )
     hist3 = TH1F( name + "_combined_fv", "Added Magnitude of Tau Four Vectors", 100, -200, 200 )
-    hist4 = TH1F( name + "_combined_boosted_fv", "Added Magnitude of Tau Four Vectors Boosted using Muon Frame, cut at 2.75 away from Z m", 100, -200, 200 )
+    hist4 = TH1F( name + "_combined_boosted_fv", "Added Magnitude of Tau Four Vectors Boosted using Muon Frame", 100, -200, 200 )
+    hist2D = TH2F( name + "_im_dr", "Invariant Mass of Z vs Tau - AntiTau, no cut", 100, 0, 120, 100, 0, 120 )
+    hist2D.GetXaxis().SetTitle("Invariant Mass (GeV)")
+    hist2D.GetYaxis().SetTitle("Tau - Antitau (GeV)")
 
     directory = os.fsencode(directory)
 
@@ -73,8 +76,6 @@ def histogram(directory, name, description):
             if i.momentum.pt() > antitau.momentum.pt():
                 antitau = i
         momentum = (muon.momentum + antimuon.momentum)
-        if(abs(momentum.m() - 91.19) > 2.75):
-            continue
         hist1.Fill(momentum.m())
         muon_boost = TLorentzVector()
         muon_boost.SetPx(momentum.px)
@@ -96,9 +97,18 @@ def histogram(directory, name, description):
         tau_fourvector.Boost((91/125)*muon_boost.BoostVector())
         antitau_fourvector.Boost((91/125)*muon_boost.BoostVector())
         hist4.Fill((tau_fourvector + antitau_fourvector).Vect().Mag())
+        if((tau_fourvector + antitau_fourvector).Vect().Mag() > 40 and (abs(momentum.m() - 91.19) < 0.5)):
+            print()
+            print()
+            for candidate in evt.particles:
+                print(candidate.pid)
+                print(candidate.momentum.e)
+                print(candidate.status)
+        hist2D.Fill(momentum.m(),(tau_fourvector + antitau_fourvector).Vect().Mag())
     hist1.Write()
     hist2.Write()
     hist3.Write()
     hist4.Write()
+    hist2D.Write()
 histogram("data","Initial study","H tau tau events")
 myfile.Close()
